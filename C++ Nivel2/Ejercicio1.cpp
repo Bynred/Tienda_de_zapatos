d506ee641fe6ccb9d2c4189762a9f21e589f60a9
Brayan Aguirre <brayanalfonso234@mail.com>
2025-06-23 01:52:33 -0500

    documento actualizado

--- C++ Nivel2/Ejercicio1.cpp
+++ C++ Nivel2/Ejercicio1.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
-#include <string>
+#include <conio.h>
 using namespace std;
 
 int main(){
     int valores[50],x;
 
-    
+    cout<<"Digite la cantidad de elementos deseados para el arreglo: ";
+    cin>>x;
+
+    for(int i=0;i<x;i++)
+    {
+        cout<<"Digite el elemento numero "<< i+1<<" del arreglo: ";
+        cin>>valores[i];
+    }
+
+    system("cls");
+
+    cout<<"Los elementos contenidos en el arreglo son: "<<endl;
+
+    for(int i=0;i<x;i++)
+    {
+        cout<<valores[i]<<endl;
+    }
+    getch();
 }
